4eaa53b852c3e47250ad684bd5f9788dc324f9e4
Junnooh <crowlergreen@gmail.com>
2023-11-26 21:45:23 -0300

    aprendendo js

---
+++ main.js
@@ -0,0 +1,13 @@
+/*window.alert ('Olá mundo!') /*PoP-up chato de quando entra na pag
+window.confirm ('Se cancelar é viado!') /* da a opção de "Ok e Cancelar
+window.prompt ('É benino ou benina? Maoii hahaha.') //Pergunta as paradas //
+// comenta em uma unica linha ( mas só funciona com javascript )
+*/
+
+window.alert ('Olá eu faço a soma de uma area!')
+window.alert ('Basta voce me passar a base e a altura!')
+
+var base = Number.parseInt(window.prompt ('Qual a base da sua area?'))
+var altura = Number.parseInt(window.prompt ('Qual a altura da sua area?'))
+var area = base * altura 
+window.alert ('Sua area é:' +area +"m²") // usar o "+ variavel" é uma concatenação
